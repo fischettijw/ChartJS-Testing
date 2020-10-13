68730b8c059c38f152b43e2389ec77a5db71da5e
Joseph Fischetti <fischettijw@gmail.com>
2020-10-13 07:12:01 -0400

    first commit

---
+++ sketch.js
@@ -0,0 +1,21 @@
+let d = [45, 10, 45, 2, 20, 30, 45];
+let ctx = document.getElementById('myChart').getContext('2d');
+let chart = new Chart(ctx, {
+    // The type of chart we want to create
+    type: 'bar',
+
+    // The data for our dataset
+    data: {
+        labels: ["January", "February", "March", "April", "May", "June", "July"],
+        datasets: [{
+            label: "My First dataset",
+            backgroundColor: 'rgb(255, 99, 132)',
+            borderColor: 'rgb(255, 99, 132)',
+            data: d,
+            // data: [0, 10, 5, 2, 20, 30, 45],
+        }]
+    },
+
+    // Configuration options go here
+    options: {}
+});
